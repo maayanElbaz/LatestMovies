2f63a4149cdbba680b2287ac148d7e81ffbf769b
maayan <maayan4336@gmail.com>
2021-12-16 11:25:07 +0200

    create views

---
+++ app/src/main/java/com/example/latestmovies/models/MovieModel.kt
@@ -0,0 +1,10 @@
+package com.example.latestmovies.models;
+
+data class MovieModel (
+
+    val original_title: String?,
+    val poster_path: String?,
+    val release_date: String?,
+    val overview: String
+
+)
